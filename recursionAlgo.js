b27264ace955325cb56e425d318e47ebb92c9cff
Mark Carthon <macarthoniii@gmail.com>
2023-06-27 12:22:16 -0400

    more recursion practise

--- recursionAlgo.js
+++ recursionAlgo.js
@@ -10,7 +10,7 @@ function recursiveSigma(number) {
     
 }
 
-console.log(`\n\nOutput of recursiveSigma(5): \n${recursiveSigma(5)}\n\n`)
+console.log(`\n\nOutput of recursiveSigma(5): \n${recursiveSigma(5)}\n\n`);
 
 function sumArray(array) {
     
@@ -24,4 +24,38 @@ function sumArray(array) {
     
 }
 
-console.log(`\n\nOutput of sumArray([1,2,3,4,5]): \n${sumArray([1, 2, 3, 4, 5])}\n\n`)
+console.log(`\n\nOutput of sumArray([1,2,3,4,5]): \n${sumArray([1, 2, 3, 4, 5])}\n\n`);
+
+
+
+
+
+function factorial(number) {
+    
+    number = Math.floor(number);
+    
+    if (number < 2) {
+        
+        return 1;
+        
+    }
+    
+    return number * factorial(number - 1);
+        
+}
+
+console.log(`\n\nOutput of factorial(5): \n${factorial(5)}\n\n`);
+
+function fibonacci(number) {
+    
+    if (number < 2) {
+        
+        return number;
+        
+    }                    
+    
+    return fibonacci(number - 1) + fibonacci(number - 2);
+    
+}
+
+console.log(`\n\nOutput of fibonacci(5): \n${fibonacci(5)}\n\n`);
